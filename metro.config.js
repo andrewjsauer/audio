d2da2c4702d2dc776c50585e6209da0bed9ad291
Andrew Sauer <andrewjohnsauer@gmail.com>
2023-12-05 14:33:51 -0800

    Adding SVG transformer

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,23 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+
+const defaultConfig = getDefaultConfig(__dirname);
+const { assetExts, sourceExts } = defaultConfig.resolver;
+
+const svgTransformerConfig = {
+  transformer: {
+    babelTransformerPath: require.resolve('react-native-svg-transformer'),
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    }),
+  },
+  resolver: {
+    assetExts: assetExts.filter((ext) => ext !== 'svg'),
+    sourceExts: [...sourceExts, 'svg'],
+  },
+};
 
 const config = {
   resolver: {
@@ -6,4 +25,7 @@ const config = {
   },
 };
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = mergeConfig(
+  mergeConfig(defaultConfig, svgTransformerConfig),
+  config,
+);
--- src/components/App/style.ts
+++ src/components/App/style.ts
@@ -2,8 +2,8 @@ import styled from 'styled-components/native';
 
 export const StyledText = styled.Text`
   color: ${(props) => props.theme.colors.text};
-  font-family: ${(props) => props.theme.fonts.regular};
-  font-size: ${(props) => props.theme.fontSizes.regular};
+  font-family: ${(props) => props.theme.fonts.extraBold};
+  font-size: ${(props) => props.theme.fontSizes.large};
 `;
 
 export const StyledView = styled.View`
