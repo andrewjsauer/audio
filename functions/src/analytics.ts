d1dbf6f82f9ae264da9148031509f99e3ced9aa9
Andrew Sauer <andrewjohnsauer@gmail.com>
2024-01-18 09:04:51 -0800

    Added new analytics BE event for tracking recordings, and finally adding them to be exported

--- functions/src/analytics.ts
+++ functions/src/analytics.ts
@@ -1,4 +1,5 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 import { Analytics } from '@segment/analytics-node';
 import moment from 'moment-timezone';
 
@@ -18,7 +19,7 @@ export const trackEvent = (event: string, userId: string, properties?: any) => {
   });
 };
 
-exports.sendNewUserCreatedEvent = functions.firestore
+export const trackNewUserCreatedEvent = functions.firestore
   .document('users/{userId}')
   .onCreate((snap) => {
     const newUser = snap.data();
@@ -61,4 +62,55 @@ exports.sendNewUserCreatedEvent = functions.firestore
     trackEvent('New User Created', id);
 
     return Promise.resolve();
+  });
+
+const didBothPartnersAnswer = async (partnershipId: string) => {
+  try {
+    const todayUTC = moment.utc().startOf('day').toDate();
+    const tomorrowUTC = moment.utc(todayUTC).add(1, 'days').toDate();
+
+    const startOfDay = admin.firestore.Timestamp.fromDate(todayUTC);
+    const endOfDay = admin.firestore.Timestamp.fromDate(tomorrowUTC);
+
+    const recordingsRef = admin.firestore().collection('recordings');
+    const query = recordingsRef
+      .where('partnershipId', '==', partnershipId)
+      .where('createdAt', '>=', startOfDay)
+      .where('createdAt', '<', endOfDay);
+
+    const recordings = await query.get();
+
+    if (recordings.size === 2) {
+      recordings.forEach((doc) => {
+        const { userId } = doc.data();
+        trackEvent('Both Partners Answered', userId);
+      });
+    }
+  } catch (error) {
+    functions.logger.error(error);
+  }
+};
+
+export const trackAnswerRecordedEvent = functions.firestore
+  .document('recordings/{recordingId}')
+  .onCreate(async (snap) => {
+    try {
+      const newRecording = snap.data();
+
+      const { userId, createdAt, duration, partnershipId } = newRecording;
+
+      const properties = {
+        createdAt: convertToDate(createdAt),
+        duration,
+        partnershipId,
+      };
+
+      trackEvent('New Answer Recorded', userId, properties);
+
+      await didBothPartnersAnswer(partnershipId);
+    } catch (error) {
+      functions.logger.error('Error in trackAnswerRecordedEvent:', error);
+    }
+
+    return null;
   });
