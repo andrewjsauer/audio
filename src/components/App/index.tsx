ce55d8a9639b101d35694930edb856624756e515
Andrew Sauer <andrewjohnsauer@gmail.com>
2024-02-01 15:12:51 -0800

    SAVEPOINT

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -80,7 +80,7 @@ function App(): JSX.Element {
 
   return (
     <Stack.Navigator>
-      {hasPreviouslySubscribed ? (
+      {true ? (
         <>
           <Stack.Screen
             component={QuestionScreen}
